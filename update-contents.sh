6b6fb34de1b27da135384a8acd715ee6d80c90de
Dmitry Senin <seninds@gmail.com>
2015-11-05 11:29:32 +0300

    Added ssh config

--- update-contents.sh
+++ update-contents.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-if ! cmp -s ~/.bashrc .bashrc ; then
-    cp -f .bashrc ~
-fi
-
-if ! cmp -s ~/.vimrc .vimrc ; then
-    cp -f .vimrc ~
-fi
-
-if [ ! -e ~/.bash ]; then
-    cp -rf .bash ~
-else
-    REPO_FILES=$(find .bash -type f)
-    for REPO_FILE in $REPO_FILES; do
-        if [ ! -e $HOME/$REPO_FILE ] || ! cmp -s $REPO_FILE $HOME/$REPO_FILE ; then
-            cp -f $REPO_FILE $HOME/$REPO_FILE
-        fi
-    done
-fi
+cp -f .bashrc ~
+cp -f .vimrc ~
+cp -rf .bash ~
+cp -rf .ssh ~
