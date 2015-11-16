a7ee0147cf341a14b383f98d4d193f3e323c2d82
Dmitry Senin <seninds@gmail.com>
2015-11-16 14:29:37 +0300

    Fixed bug in docker install

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -2,8 +2,12 @@
 
 # install latest docker version
 sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo bash -c "echo deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -c | cut -d$'\t' -f 2) main > /etc/apt/sources.list.d/docker.list"
+
+DOCKER_LIST="/etc/apt/sources.list.d/docker.list"
+if [ ! -e  "$DOCKER_LIST" ] || grep -q "apt.dockerproject.org" "$DOCKER_LIST" ; then
+    sudo bash -c "echo deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -c | cut -d$'\t' -f 2) main > \"$DOCKER_LIST\""
+fi
+
 sudo apt-get update
 sudo apt-get purge lxc-docker* docker.io -y
-sudo apt-get upgrade -y
-sudo apt-get install docker-engine -y
+sudo apt-get install docker-engine -y --force-yes
