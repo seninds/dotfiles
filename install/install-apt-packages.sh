8b20ae663f3e23ad7a6356bd8373a4fdde979c0a
Dmitry Senin <seninds@gmail.com>
2015-11-29 20:43:47 +0300

    Added python and apt packages to install

--- install/install-apt-packages.sh
+++ install/install-apt-packages.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt-get install python ipython python3 ipython3 git
+sudo apt-get install -y python3 ipython3 python3-ipdb python3-requests
+sudo apt-get install -y python ipython python-ipdb python-requests
+sudo apt-get install -y git curl rsync make tmux
