c8b653641b5e0b40c507bcb9ec138f6f091c3f2b
Dmitry Senin <seninds@gmail.com>
2015-12-03 09:35:11 +0300

    Renamed files; Added vim-autopep8 plugin

---
+++ install/vim-plugins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a VIM_PLUGINS_REPOS=("https://github.com/nvie/vim-flake8"
+                              "https://github.com/klen/python-mode"
+                              "https://github.com/tell-k/vim-autopep8")
+
+VIM_BUNDLE_DIR=~/.vim/bundle
+for PLUGIN_REPO in "${VIM_PLUGINS_REPOS[@]}"; do
+    LOCAL_REPO_PATH="$VIM_BUNDLE_DIR"/$(basename "$PLUGIN_REPO")
+    if [ -e "$LOCAL_REPO_PATH" ]; then
+        echo "updating repo '$PLUGIN_REPO' in '$LOCAL_REPO_PATH'..."
+        git pull "$LOCAL_REPO_PATH"
+    else
+        echo "cloning repo '$PLUGIN_REPO' to '$LOCAL_REPO_PATH'..."
+        git clone "$PLUGIN_REPO" "$LOCAL_REPO_PATH"
+    fi
+done
