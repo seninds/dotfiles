866a3381f250f2778f2dbdd907ae1684658a169d
Dmitry Senin <seninds@gmail.com>
2016-01-08 08:11:36 -0500

    Added force-yes to apt-packages.sh

--- install/apt-packages.sh
+++ install/apt-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install -y python3 ipython3 python3-ipdb python3-requests
-sudo apt-get install -y python ipython python-ipdb python-requests
-sudo apt-get install -y git curl rsync make tmux
+sudo apt-get install -y --force-yes python3 ipython3 python3-ipdb python3-requests
+sudo apt-get install -y --force-yes python ipython python-ipdb python-requests
+sudo apt-get install -y --force-yes git curl rsync make tmux
