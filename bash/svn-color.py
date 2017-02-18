d9689380f027158bfff682cb46b4e570517adb63
Dmitry Senin <seninds@gmail.com>
2017-02-18 11:08:56 +0300

    Fix svn-color

--- bash/svn-color.py
+++ bash/svn-color.py
@@ -6,21 +6,21 @@ import subprocess
 
 
 status_colors = {
-    "^\s*M\s+": "31",     # red
-    "^\s*\?\s+": "37",    # grey
-    "^\s*A\s+": "32",     # green
-    "^\s*X\s+": "33",     # yellow
-    "^\s*C\s+": "30;41",  # black on red
-    "^-[^-]": "31",       # red
-    "^\s*D\s+": "31;1",   # bold red
-    "^\+[^+]": "32",      # green
+    '^\s*M\s+': '31',     # red
+    '^\s*\?\s+': '37',    # grey
+    '^\s*A\s+': '32',     # green
+    '^\s*X\s+': '33',     # yellow
+    '^\s*C\s+': '30;41',  # black on red
+    '^-[^-]': '31',       # red
+    '^\s*D\s+': '31;1',   # bold red
+    '^\+[^+]': '32',      # green
 }
 
 
 def colorize(line):
     for s, color in status_colors.items():
         if re.match(s, line):
-            return ''.join(("\001\033[", color, "m", line, "\033[m\002"))
+            return ''.join(('\033[', color, 'm', line, '\033[m'))
     return line
 
 
@@ -35,13 +35,13 @@ def quote(arg):
     return '"%s"' % escape(arg) if ' ' in arg else arg
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args_list = [quote(arg) for arg in sys.argv[1:]]
     cmd = ' '.join(['svn'] + args_list)
     output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
     tabsize = 4
-    colorized_cmds = ('st', 'status', 'add', 'rm', 'remove', 'diff', 'log')
+    colorized_cmds = ('st', 'stat', 'status', 'add', 'rm', 'remove', 'diff', 'log')
     for line in output.stdout:
         line = line.expandtabs(tabsize)
         if(sys.argv[1] in colorized_cmds):
--- bashrc
+++ bashrc
@@ -10,4 +10,3 @@ if [ -f ~/.bash/bash_custom ]; then . ~/.bash/bash_custom; fi
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
-
