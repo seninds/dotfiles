d4cd3eac0145a2aef3d1c28ae10f30020c5c7f4d
Dmitry Senin <seninds@gmail.com>
2017-05-07 20:45:51 +0300

    Del install dir

--- install/common/apt-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install -y --force-yes python3 python3-pip ipython3 python3-ipdb python3-requests
-sudo apt-get install -y --force-yes python python-pip ipython python-ipdb python-requests
-sudo apt-get install -y --force-yes git curl rsync make tmux vim
--- install/mongodb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-MONGODB_LIST="/etc/apt/sources.list.d/mongodb-org-3.0.list"
-echo "deb http://repo.mongodb.org/apt/ubuntu $(lsb_release -c | cut -f2)/mongodb-org/3.0 multiverse" | tee "$MONGODB_LIST"
-apt-get update
-apt-get install -y mongodb-org
